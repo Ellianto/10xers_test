74a8ed88a0d401eaeb084ff020073ea4204f9d75
Ellianto <ellianto@student.umn.ac.id>
2020-11-27 23:10:11 +0700

    FInalized Sticky Carousel Header

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './CustomHeader.css';
 
 import Navbar from 'react-bootstrap/Navbar';
+import {Sticky, StickyContainer} from 'react-sticky';
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import {Carousel} from 'react-responsive-carousel';
@@ -13,43 +14,42 @@ function CustomHeader(props){
     const topMovieList = props.topMovies;
 
     return (
+        
         !topMovieList ? null :
-        <Navbar sticky='top' className='p-0'>
-            {
-                topMovieList.length <= 0 ? null :
-                <div className='header-container'>
-                    <Carousel
-                        autoPlay={true}
-                        infiniteLoop={true}
-                        showArrows={false}
-                        showIndicators={false}
-                        showStatus={false}
-                        showThumbs={false}
-                        stopOnHover={false}
-                        swipable={false}
-                    >
-                        {
-                            topMovieList.map(movie => (
-                                <div key={movie.id}>
-                                    <img
-                                        className="d-block w-100"
-                                        style={{objectFit:'cover', objectPosition:'right center'}}
-                                        src={getMovieBackdropUrl(movie.backdrop_path)}
-                                        alt={movie.title + " Backdrop Image"}
-                                        height={300}
-                                    />
-                                </div>
-                            ))
-                        }
-                    </Carousel>
-                    <div className='header-gradient'>
-                        <div className='header-element'>
-                            <h1> MovieList </h1>
-                        </div>
+        topMovieList.length <= 0 ? null :
+        <div className='sticky-header'>
+            <div className='header-container'>
+                <Carousel
+                    autoPlay={true}
+                    infiniteLoop={true}
+                    showArrows={false}
+                    showIndicators={false}
+                    showStatus={false}
+                    showThumbs={false}
+                    stopOnHover={false}
+                    swipable={false}
+                >
+                    {
+                        topMovieList.map(movie => (
+                            <div key={movie.id}>
+                                <img
+                                    className="d-block w-100"
+                                    style={{objectFit:'cover', objectPosition:'right center'}}
+                                    src={getMovieBackdropUrl(movie.backdrop_path)}
+                                    alt={movie.title + " Backdrop Image"}
+                                    height={512}
+                                />
+                            </div>
+                        ))
+                    }
+                </Carousel>
+                <div className='header-gradient'>
+                    <div className='header-element'>
+                        <h1> MovieList </h1>
                     </div>
                 </div>
-            }
-        </Navbar>
+            </div>
+        </div>
     )
 }
 
